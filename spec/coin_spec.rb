64c02c4b1dcece002b57020a425e51bf83deabbc
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-12-06 13:07:51 +0530

    Corrects a grammatical typo

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -2,7 +2,7 @@ describe Coin do
   describe "#initialize" do
     let(:coin) { Coin.new(name: "ten pence", denomination: 0.1, quantity: 10) }
 
-    context "Given an coin with name, denomination and quantity" do
+    context "Given a coin with name, denomination and quantity" do
       it "should set the coin related attributes appropriately" do
         expect(coin.name).to eq("ten pence")
         expect(coin.denomination).to eq(0.1)
