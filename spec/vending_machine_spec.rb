705892f5e83a0e3619f40a1b86f6281ee8f1dbb9
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-12-07 02:30:58 +0530

    Rearranges the specs to make them more cleaner
    The earlier specs seemed to apply to all the test cases which was
    unnecessary. With this refactoring the test setup is better organized
    based for each category of tests

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -20,70 +20,106 @@ describe VendingMachine do
 
   describe "#purchase_item" do
 
-    before do
-      item1 = Item.new(name: "Cadbury dairy milk", cost: 0.5, quantity: 0 )
-      item2 = Item.new(name: "Cadbury Freddo 18G", cost: 0.25, quantity: 8)
-      item3 = Item.new(name: "Cadburys Wispa Stickpack 27G", cost: 0.33, quantity: 12)
-      item4 = Item.new(name: "Maltesers", cost: 2, quantity: 18)
-      item5 = Item.new(name: "Cadbury Dairy Milk Giant Buttons 119G", cost: 1, quantity: 14)
+    context "vending machine has stock issues" do
 
-      one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 30)
-      two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 30)
-      five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 10)
-      ten_pence_coins = Coin.new(name: "ten pence", denomination: 0.10, quantity: 10)
-      twenty_pence_coins = Coin.new(name: "twenty pence", denomination: 0.20, quantity: 10)
-      fifty_pence_coins = Coin.new(name: "fifty pence", denomination: 0.5, quantity: 10)
-      one_pound_coins = Coin.new(name: "one pound", denomination: 1, quantity: 10)
-      two_pound_coins = Coin.new(name: "two pound", denomination: 2, quantity: 10)
+      before do
+        item1 = Item.new(name: "Cadbury dairy milk", cost: 0.5, quantity: 0 )
+        item2 = Item.new(name: "Cadbury Freddo 18G", cost: 0.25, quantity: 8)
+        item3 = Item.new(name: "Cadburys Wispa Stickpack 27G", cost: 0.33, quantity: 12)
+        item4 = Item.new(name: "Maltesers", cost: 2, quantity: 18)
+        item5 = Item.new(name: "Cadbury Dairy Milk Giant Buttons 119G", cost: 1, quantity: 14)
 
-      items = [item1, item2, item3, item4, item5]
-      coins = [one_penny_coins, two_pence_coins, five_pence_coins, ten_pence_coins,
-               twenty_pence_coins, fifty_pence_coins, one_pound_coins, two_pound_coins]
+        one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 30)
+        two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 30)
+        five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 10)
+        ten_pence_coins = Coin.new(name: "ten pence", denomination: 0.10, quantity: 10)
+        twenty_pence_coins = Coin.new(name: "twenty pence", denomination: 0.20, quantity: 10)
+        fifty_pence_coins = Coin.new(name: "fifty pence", denomination: 0.5, quantity: 10)
+        one_pound_coins = Coin.new(name: "one pound", denomination: 1, quantity: 10)
+        two_pound_coins = Coin.new(name: "two pound", denomination: 2, quantity: 10)
 
-      @vending_machine = VendingMachine.new(serial_number: "A235078BD9", items: items,
-                                       coins: coins)
+        items = [item1, item2, item3, item4, item5]
+        coins = [one_penny_coins, two_pence_coins, five_pence_coins, ten_pence_coins,
+                 twenty_pence_coins, fifty_pence_coins, one_pound_coins, two_pound_coins]
 
-      customer_one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 3)
-      customer_two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 1)
-      customer_five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 6)
+        @vending_machine = VendingMachine.new(serial_number: "A235078BD9", items: items,
+                                         coins: coins)
 
-      @amount_inserted_into_vending_machine = [customer_one_penny_coins,
-                                              customer_two_pence_coins,
-                                              customer_five_pence_coins]
-    end
-
-
-
-    context "desired item is not stocked in the vending machine for sale " do
-      let(:cadbury_eclairs) { Item.new(name: "Cadbury Eclairs", cost: 0.25, quantity: 8) }
-
-      it "should raise an appropriate error" do
-        customer_selected_item_name = "Cadburys  Eclairs"
-
-        expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
-          .to raise_error("This item is currently not available for puchase from  the vending machine")
-      end
-    end
-
-    context "user has entered amount in an appropriate denomination" do
-      it "should raise an appropriate error" do
-        customer_selected_item_name = "Cadbury dairy milk"
-
-        expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
-          .to raise_error("The desired quantity wrt this item is not currently available")
-      end
-    end
-
-    context "desired item is out of stock" do
-      it "should raise an appropriate error" do
-        customer_selected_item_name = "Cadbury dairy milk"
         customer_one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 3)
-        customer_twenty_five_pence_coins = Coin.new(name: "twenty five pence", denomination: 0.25, quantity: 1)
-        @amount_inserted_into_vending_machine = [customer_one_penny_coins,
-                                                 customer_twenty_five_pence_coins]
+        customer_two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 1)
+        customer_five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 6)
 
-        expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
-          .to raise_error("User has entered entered the amount denominations that are not accepted by the machine")
+        @amount_inserted_into_vending_machine = [customer_one_penny_coins,
+                                                customer_two_pence_coins,
+                                                customer_five_pence_coins]
+      end
+
+      context "desired item is not stocked in the vending machine for sale " do
+        let(:cadbury_eclairs) { Item.new(name: "Cadbury Eclairs", cost: 0.25, quantity: 8) }
+
+        it "should raise an appropriate error" do
+          customer_selected_item_name = "Cadburys  Eclairs"
+
+          expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
+            .to raise_error("This item is currently not available for puchase from  the vending machine")
+        end
+      end
+
+      context "desired item is out of stock" do
+        it "should raise an appropriate error" do
+          customer_selected_item_name = "Cadbury dairy milk"
+          customer_one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 3)
+          customer_twenty_five_pence_coins = Coin.new(name: "twenty five pence", denomination: 0.25, quantity: 1)
+          @amount_inserted_into_vending_machine = [customer_one_penny_coins,
+                                                   customer_twenty_five_pence_coins]
+
+          expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
+            .to raise_error("User has entered entered the amount denominations that are not accepted by the machine")
+        end
+      end
+
+    end
+
+    context "user enters incorrect currency to make a purchase" do
+      before do
+        item1 = Item.new(name: "Cadbury dairy milk", cost: 0.5, quantity: 0 )
+        item2 = Item.new(name: "Cadbury Freddo 18G", cost: 0.25, quantity: 8)
+        item3 = Item.new(name: "Cadburys Wispa Stickpack 27G", cost: 0.33, quantity: 12)
+        item4 = Item.new(name: "Maltesers", cost: 2, quantity: 18)
+        item5 = Item.new(name: "Cadbury Dairy Milk Giant Buttons 119G", cost: 1, quantity: 14)
+
+        one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 30)
+        two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 30)
+        five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 10)
+        ten_pence_coins = Coin.new(name: "ten pence", denomination: 0.10, quantity: 10)
+        twenty_pence_coins = Coin.new(name: "twenty pence", denomination: 0.20, quantity: 10)
+        fifty_pence_coins = Coin.new(name: "fifty pence", denomination: 0.5, quantity: 10)
+        one_pound_coins = Coin.new(name: "one pound", denomination: 1, quantity: 10)
+        two_pound_coins = Coin.new(name: "two pound", denomination: 2, quantity: 10)
+
+        items = [item1, item2, item3, item4, item5]
+        coins = [one_penny_coins, two_pence_coins, five_pence_coins, ten_pence_coins,
+                 twenty_pence_coins, fifty_pence_coins, one_pound_coins, two_pound_coins]
+
+        @vending_machine = VendingMachine.new(serial_number: "A235078BD9", items: items,
+                                         coins: coins)
+
+        customer_one_penny_coins = Coin.new(name: "one penny", denomination: 0.01, quantity: 3)
+        customer_two_pence_coins = Coin.new(name: "two pence", denomination: 0.02, quantity: 1)
+        customer_five_pence_coins = Coin.new(name: "five pence", denomination: 0.05, quantity: 6)
+
+        @amount_inserted_into_vending_machine = [customer_one_penny_coins,
+                                                customer_two_pence_coins,
+                                                customer_five_pence_coins]
+      end
+
+      context "user has entered amount in an appropriate denomination" do
+        it "should raise an appropriate error" do
+          customer_selected_item_name = "Cadbury dairy milk"
+
+          expect { @vending_machine.purchase_item(customer_selected_item_name, @amount_inserted_into_vending_machine) }
+            .to raise_error("The desired quantity wrt this item is not currently available")
+        end
       end
     end
 
@@ -129,7 +165,7 @@ describe VendingMachine do
         expect(@item3.quantity).to eq(11)
       end
 
-      it "should credit the amount recieved to vending machine's account" do
+      it "should credit the amount received to vending machine's account" do
         @vending_machine.purchase_item(@customer_selected_item_name,
                                        @amount_inserted_into_vending_machine)
 
@@ -199,7 +235,7 @@ describe VendingMachine do
           expect(@item3.quantity).to eq(11)
         end
 
-        it "should credit the amount recieved to vending machine's account" do
+        it "should credit the amount received to vending machine's account" do
           @vending_machine.purchase_item(@customer_selected_item_name,
                                          @amount_inserted_into_vending_machine)
 
@@ -260,7 +296,7 @@ describe VendingMachine do
           expect(@item3.quantity).to eq(12)
         end
 
-        it "should credit the amount recieved to vending machine's account" do
+        it "should credit the amount received to vending machine's account" do
           @vending_machine.purchase_item(@customer_selected_item_name,
                                          @amount_inserted_into_vending_machine)
 
@@ -277,4 +313,5 @@ describe VendingMachine do
     end
 
   end
+
 end
