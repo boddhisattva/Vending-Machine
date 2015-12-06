72f4ed6510e2b3bc4d9b056c5a9ad349096bad49
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-12-07 00:38:16 +0530

    Allows one to update quantity based on a purchased item
    Item quantity decreases and coin quantity increases with every
    successful purchase. Hence quantity should be writable and readable.

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,9 +1,11 @@
 class Coin
 
-  attr_reader :name, :denomination, :quantity
+  attr_reader :name, :denomination
+  attr_accessor :quantity
 
   def initialize(name:, denomination:, quantity:)
     @name, @denomination = name, denomination
     @quantity = quantity
   end
+
 end
--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,9 @@
 class Item
 
-  attr_reader :name, :cost, :quantity
+  attr_reader :name, :cost
+  attr_accessor :quantity
 
   def initialize(name:, cost:, quantity: )
     @name, @cost, @quantity = name, cost, quantity
   end
-
 end
